d56f3eb151bcc6c6c9d4b2f6f64f9d0fb52c6105
Damoun Nassehi <damoun.nassehi@gmail.com>
2025-05-23 13:19:00 +0200

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,9 +1,12 @@
+// Server-side Admin-SDK for Firebase
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
+  // Fire bas64-kodet JSON fra env-variabel
   const serviceAccount = JSON.parse(
     Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!, 'base64').toString()
   );
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
